790c716c7c501a793ed26b4a77c85e59d91bd090
Shresth Rajput <rajshresth2@gmail.com>
2024-08-07 11:39:57 +0530

    Lecture on GEC Basic

---
+++ Day-7 Scopes/scope.js
@@ -0,0 +1,14 @@
+var a=1;
+let b=2;
+const c=3;
+{
+    var a=10;
+    let b=20;
+    const c=30;
+    console.log(`Inside Block ${a}`)
+    console.log(`Inside Block ${b}`)
+    console.log(`Inside Block ${c}`)
+}
+console.log(`Outside Block ${a}`)
+console.log(`Outside Block ${b}`)
+console.log(`Outside Block ${c}`)
