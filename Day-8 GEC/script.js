adcc5adc96da1b19b035255f456dcf7149ea6e7e
Shresth Rajput <rajshresth2@gmail.com>
2024-08-12 11:40:51 +0530

    Lecture on GEC

--- Day-8 GEC/script.js
+++ Day-8 GEC/script.js
@@ -5,3 +5,52 @@
 // console.log(b)
 // console.log(c)
 
+/* Question-1
+var a=1;
+let b=2;
+const c=3;
+{
+    var a=10;
+    let b=20;
+    const c=30;
+    var d=40;
+    console.log(`Inside Block ${a}`)
+    console.log(`Inside Block ${b}`)
+    console.log(`Inside Block ${c}`)
+    console.log(`Inside Block ${d}`)    
+}
+console.log(`Outside Block ${a}`)
+console.log(`Outside Block ${b}`)
+console.log(`Outside Block ${c}`)
+console.log(`Outside Block ${d}`) */
+
+/* Question-2 
+let a=5;
+const b=6;
+    {
+        let a=100;
+        const b=200
+        console.log(a)
+        console.log(b)
+        a=50;
+    }
+console.log(a)
+console.log(b)*/
+
+
+/* Question-3
+let a="Hello"
+    if(true)
+    {
+        let a="Haha"
+        var b="Hey"
+        console.log(a)
+        console.log(b)
+    }
+console.log(a)
+console.log(b)  */
+
+
+
+
+
