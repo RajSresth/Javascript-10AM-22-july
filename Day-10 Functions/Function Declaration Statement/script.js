dadde5ca2b2a57c56f9b52288c6d27821e35eac4
Shresth Rajput <rajshresth2@gmail.com>
2024-08-21 11:54:32 +0530

    Lecture on Function Basics

---
+++ Day-10 Functions/Function Declaration Statement/script.js
@@ -0,0 +1,77 @@
+/*function call(name)
+{
+    console.log(`Kya me ${name} apse kuch maang sakta hu..`);
+    console.log("Sachmuch mang sakta hu..")
+
+    return "Bhaiyo aur Behno..!!"
+}
+
+// call("Rohit")
+console.log(call("Virat")) */
+
+/*function callSalman(name)
+{
+    console.log("Hud Hud Dabang..!!")
+    return `${name}, woh sirf meri h..!`
+}
+
+
+let result=callSalman("Katrina")
+
+function callShera(){
+    console.log("Salmon Bhai shera is speaking..");
+    console.log(result);
+    return `Bhai lagta h apke pyar me gir gayi..!!`
+}
+let res2=callShera()
+console.log(res2) */
+
+/*
+function raju(){
+    return `Zor Zor se bolke sabko scheme batade`
+}
+
+let res1=raju()
+console.log(res1)
+
+function babuRao(){
+    return `Me bhi totla, mela baap bhi totla, mela pula khandan totla`
+}
+console.log(babuRao())*/
+
+/*
+function ashish(){
+    console.log("Bijli ka Bill tera baap bharega..")
+}
+console.log(ashish())
+*/
+
+/*
+function message(){
+    console.log("Bulati h magar jane ka nahi..")
+
+    return `Sigma`
+
+    return `Sakth Londa`
+}
+
+console.log(message()) */
+
+
+/*
+function findPower(a){
+    return (a*a*a)
+}
+
+
+let result= findPower(5) - findPower(3)
+console.log(result) */
+
+
+
+something()
+function something(){
+    return 
+    console.log("Biscut...nahi viss kut");
+}
+
---
+++ Day-9 Type Coercion/Implicit Type Coercion/implicit.js
@@ -0,0 +1,4 @@
+let a=1;
+console.log(isNaN(a))
+
+
