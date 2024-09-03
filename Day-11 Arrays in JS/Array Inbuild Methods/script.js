425ae35d40dcc213121f1f3dc311eddad306acf2
Shresth Rajput <rajshresth2@gmail.com>
2024-09-03 11:43:27 +0530

    Array Inbuilt Methods

---
+++ Day-11 Arrays in JS/Array Inbuild Methods/script.js
@@ -0,0 +1,85 @@
+/*1. push()
+const arr=[1,2,3,4,5]
+
+const result=arr.push(10)
+console.log(arr)
+console.log(result) */
+
+/* pop()
+const arr=[1,2,3,4,5,16] 
+const res=arr.pop()
+console.log(arr)
+console.log(res) */
+
+
+/* unshift() 
+const users=["John","Troy"];
+const res=users.unshift("Tinku")
+console.log(users)
+console.log(res) */
+
+/* shift() 
+const users=["Tinku","John","Troy"];
+const res=users.shift()
+console.log(users)
+console.log(res)*/
+
+/* splice(a,b,c) 
+// Insertion
+
+const arr=[1,2,5,6]
+const res=arr.splice(2,0,3,4)
+console.log(arr)
+console.log(res) 
+
+//Deletion
+const comedy=["Dhamal","Tubelight","Race-3","Welcome"]
+comedy.splice(1,2)
+console.log(comedy)
+
+
+//updaction
+const comedy=["Dhamal","Tubelight","PHP","Race-3","Welcome"]
+comedy.splice(1,3,"PHP")
+console.log(comedy)*/
+
+/*
+const yt=["Carry","Ashish","Harsh","Sandeep Maheshwari","Vivek Bindra","Elvish Yadav","","BB","Purav"]
+const speaker=yt.slice(a=3,b=5)
+console.log(speaker) */
+
+/*const users=["Tinku","Chombi","Komal","Neeraj"]
+const users=["Tinku","Chombi","Komal"]
+
+
+// const females=users.slice(a=1,b=3)
+const females=users.slice(a=1)
+console.log(females)
+console.log(users) */
+
+/*
+const users=["Tinku","Chombi","Chombu","Komal"]
+const myArray=users.slice(1)
+// ["Chombi","Chombu","Komal"]
+myArray.splice(1,1)
+console.log(myArray)
+*/
+
+
+/* reverse()
+const arr=[1,2,3,4,5]
+console.log(arr.reverse()) */
+
+
+/*indexOf()
+
+const arr=["Ravi","Sunny","Raj","Sunny"]
+
+// console.log(arr.indexOf("John"))   //:- index of an element -1
+console.log(arr.indexOf("Sunny",2)) */
+
+/* includes()
+const arr=["Ravi","Sunny","Raj","Mohit","Komal"]
+console.log(arr.includes("Komal")) */
+
+
---
+++ Day-11 Arrays in JS/Ways to Create an Array/script.js
@@ -0,0 +1,15 @@
+/*let movies=[10,20,30]
+console.log(movies) */
+
+// By using new keyword and Array() constructor
+
+// Empty array
+const arr=new Array();
+console.log(arr)
+
+
+// Array with literals
+
+const users=new Array("Ravi","Suraj","Amit")
+console.log(users[2])
+console.log(users[users.length-1])
