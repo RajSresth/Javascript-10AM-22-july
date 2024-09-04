1718cfcff09855a05b01d160860d9e72370fc661
Shresth Rajput <rajshresth2@gmail.com>
2024-09-04 11:43:50 +0530

    Array Completed

--- Day-11 Arrays in JS/Array Inbuild Methods/script.js
+++ Day-11 Arrays in JS/Array Inbuild Methods/script.js
@@ -83,3 +83,105 @@ const arr=["Ravi","Sunny","Raj","Mohit","Komal"]
 console.log(arr.includes("Komal")) */
 
 
+/*
+const arr=[5,3,4,2,1,10]
+const res=arr.sort((a,b)=>a-b) //Ascending Order
+// arr.sort((a,b)=>b-a) Descending Order
+
+console.log(arr) 
+console.log(res)*/
+
+// foreach(callback)
+
+/*
+let arr=[1,2,3,4,5]   //output:[10,20,30,40,50]
+let output=[]         //empty array
+
+const res=arr.forEach((val,index,arr)=>{
+    output.push(val*10)
+})
+
+console.log(output)
+console.log(res) */
+
+/*
+let arr=[5,15,25,35,45]
+const output=[]
+
+arr.forEach(val=>{
+    output.push(Math.floor(val/2))
+})
+
+console.log(output) */
+
+/*
+let arr=[10,20,30,40,50]
+let sum=0;
+
+arr.forEach(val=>{
+    sum=sum+val;
+})
+
+console.log(`Sum is: ${sum}`) */
+
+/*
+const arr=[1,2,3,4,5]
+
+arr.forEach((val,i,array)=>{
+     array[i]=val**2
+})
+console.log(arr)
+
+arr.forEach((val,i,array)=>{
+    array[i]=Math.sqrt(val)
+})
+console.log(arr) */
+
+/*
+let arr=[1,2,3,4,5]
+let sum=0;
+const output=arr.map((val,index,array)=>{
+    sum=sum+val
+})
+console.log(sum)  */
+
+/*
+let arr=[1,2,3,4,5]
+let output=[]
+output.push(arr.reduce((acc,val,index,array)=>{
+    acc=acc+val;   
+    return acc;
+},0))
+      
+console.log(output) */
+
+
+
+/* filter() Method
+let arr=[10,20,30,40,50];
+const res=arr.filter((val,index,array)=>val>30)
+console.log(res) */
+
+
+/*flat() Method
+let arr=[1,2,[3,4],5,[6,7,[8,9]],10]
+const res=arr.flat(2)
+console.log(res)
+*/
+
+/*Array.isArray() Method
+
+let arr=[]
+let arr1={}
+let arr2="String"
+console.log(Array.isArray(arr))
+console.log(Array.isArray(arr1))
+console.log(Array.isArray(arr2)) */
+
+/* Array.from() Method
+let name="Ravi"
+const res=Array.from(name)
+console.log(res)
+*/
+
+
