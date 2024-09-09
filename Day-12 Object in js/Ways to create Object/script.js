4c5366a36976a1e2932b09407fe88a5b9c163633
Shresth Rajput <rajshresth2@gmail.com>
2024-09-09 15:07:14 +0530

    Object Methods and Deep Copy

--- Day-12 Object in js/Ways to create Object/script.js
+++ Day-12 Object in js/Ways to create Object/script.js
@@ -68,14 +68,98 @@ else{
     console.log("Aee bille tera cake tera bhai le gaya")
 } */
 
+/*
+   const jack={
+                cake:"Chocolate",
+                price:'$5'
+                }
 
-    const obj={
-        name:"Tinku",
-        age:24,
-        city:"Noida"
-    }
+  const oggy={}  //Empty Object
 
-    for(val in obj)
-    {
-        console.log(val)
+  for(element in jack)
+  {
+    oggy[element]=jack[element]
+  }
+
+  delete oggy.cake;
+
+  console.log(oggy)
+  console.log(jack)   */
+
+/* Deep copy in array  using:- for in loop
+  const arr1=[10,20,30];
+  const arr2=[];
+
+  for(index in arr1)
+  {
+    arr2[index]=arr1[index]
+  }
+
+  console.log(arr2) */
+
+
+
+/* Deep Copy in Array Using for of loop
+
+const users=["Ravi","Shubham","Komal"];
+
+const newArr=[]
+
+for(val of users)
+{
+    // console.log(val)
+    newArr.push(val)
+}
+
+newArr.push("Nisha")
+console.log(users)
+console.log(newArr)*/
+
+
+/* Behaviours (Method) inside an Object
+
+const actor={
+    name:'Amir Khan',
+    dialog:function(msg){
+        console.log(`Eee wrang namber h..!!`)
+        console.log(msg)
+        return `Pk h Ka`
     }
+}
+
+// How to call a method
+const res=actor.dialog(`Mari choriyan choro se kam h ke`)
+console.log(res) */
+
+
+// create a method using arrow function
+const singers=[
+                {
+                    name:'kk',
+                    song:()=>console.log(`Ajab si`)
+                },
+                {
+                    name:'Himesh',
+                    song:()=>console.log(`Onnnnnnn Hooosur tera tera teraaaan suroooor..`)
+                },
+                {
+                    name:'Gian',
+                    song:()=>console.log(`Me hu gian`)
+                }
+            ]
+
+singers.map(val=>{
+        if(val.name==="Himesh")
+        {
+            val.song()
+        }
+})
+
+    
+
+
+
+
+ 
+
+                
