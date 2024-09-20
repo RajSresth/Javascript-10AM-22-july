6e6490d7139c35d078e5c02d476abc01f72e4ea3
Shresth Rajput <rajshresth2@gmail.com>
2024-09-20 11:06:25 +0530

    Mouse Events Complete

--- Day-18 Events in JS/Mouse Events/script.js
+++ Day-18 Events in JS/Mouse Events/script.js
@@ -1,27 +1,50 @@
 const button=document.querySelector("button")
 
+
 /* Single Click*/
   button.addEventListener("click",()=>{
-     alert("Button is Clicked..!!")
+     console.log("Button Left Clicked..!!")
  }) 
 
-/* double click*/
+/* double click
 button.addEventListener("dblclick",()=>{
     console.log("Jhukega Nahi..!!")
-}) 
+}) */
 
-/* mouse down*/
+/* mouse down
 button.addEventListener("mousedown",()=>{
     console.log("Mouse is Pressed..!!")
-})
+})*/
 
-/* mouse up*/
+/* mouse up
 button.addEventListener("mouseup",()=>{
     console.log("Mouse is Released")
-})
+})*/
 
 
-/* mouse move*/
+/* mouse move
 button.addEventListener("mousemove",(e)=>{
 console.log(`Mouse moved: X=${e.clientX}, Y=${e.clientY}`)
+})*/
+
+
+/* mouseover 
+button.addEventListener("mouseenter",()=>{
+    document.body.style.backgroundColor="black"
+})*/
+
+/* mouseout
+button.addEventListener("mouseleave",()=>{
+    document.body.style.backgroundColor="white"
+})*/
+
+/* contextmenu right click
+button.addEventListener("contextmenu",(e)=>{
+    e.preventDefault()
+    console.log("Button Right Clicked")
+})*/
+
+/*wheel*/
+window.addEventListener("wheel",(e)=>{
+    console.log(e.deltaY)
 })
