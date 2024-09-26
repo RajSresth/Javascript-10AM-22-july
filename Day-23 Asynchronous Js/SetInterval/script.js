34162c388c79b1399a837c368ff9adffe0aa7fe2
Shresth Rajput <rajshresth2@gmail.com>
2024-09-26 11:33:22 +0530

    Introduction to Asynchronous Javascript

---
+++ Day-23 Asynchronous Js/SetInterval/script.js
@@ -0,0 +1,8 @@
+let count=1;
+function dialog()
+{
+    console.log(`chapri Nalle berozgar-${count++}`)
+}
+
+
+setInterval(dialog, 2000);
---
+++ Day-23 Asynchronous Js/SetTimeOut/script.js
@@ -0,0 +1,16 @@
+function dialog(){
+    console.log("Abe jaldi bol kal subha palvel nikalna h..!!")
+}
+
+let a=1;
+let b=2;
+let c=3;
+let d=4;
+let e=5;
+
+console.log(a)
+console.log(b)
+console.log(c)
+                    setTimeout(dialog,5000);
+console.log(d)
+console.log(e)
